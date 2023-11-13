db5637b4e1adeb4647616bd5450742d595d7fadd
kadealicious <kadesamson@gmail.com>
2023-11-12 18:10:59 -0800

    Quad drawing, debug plugins

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use bevy::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+
 pub mod test;
 
 fn main() {
     let mut juicebox: App = App::new();
-	juicebox.add_plugins((DefaultPlugins, test::HelloWorld));
+	juicebox.add_plugins
+	((
+		DefaultPlugins, 
+		test::HelloWorld, 
+		
+		// Debug plugins: 
+		LogDiagnosticsPlugin::default(), 
+		FrameTimeDiagnosticsPlugin::default(), 
+	));
 	juicebox.run();
 }
-
-// comment
--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::sprite::{Mesh2dHandle, MaterialMesh2dBundle};
 
 pub struct HelloWorld;
 impl Plugin for HelloWorld
@@ -7,6 +8,7 @@ impl Plugin for HelloWorld
 	{
 		app.insert_resource(GreetingTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
 		app.add_systems(Update, hello_world);
+		app.add_systems(Startup, test_setup);
 	}
 }
 
@@ -21,4 +23,43 @@ fn hello_world(time: Res<Time>, mut timer: ResMut<GreetingTimer>)
 	}
 }
 
-// comment
+fn test_setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, asset_server: Res<AssetServer>)
+{
+	// Set up triangle mesh.
+	let texture_handle				= asset_server.load("test.png");
+	let quad_mesh: Mesh				= create_quad();
+	let mesh_handle: Mesh2dHandle	= meshes.add(quad_mesh).into();
+	
+	// Spawn camera and triangle.
+	commands.spawn(Camera2dBundle::default());
+	commands.spawn(MaterialMesh2dBundle
+	{
+		mesh:		mesh_handle.clone(), 
+		transform:	Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)).with_scale(Vec3::splat(512.0)), 
+		material:	materials.add(ColorMaterial::from(texture_handle)), 
+		..default()
+	});
+}
+
+fn create_quad() -> Mesh
+{
+	// Create default quad mesh with vertex positions & vertex indices.
+	let mut mesh = Mesh::from(shape::Quad::default());
+	
+	// Insert vertex color attributes for each of the 4 vertices of the mesh.
+	mesh.insert_attribute
+	(
+		Mesh::ATTRIBUTE_COLOR, 
+		vec!
+		[
+			Color::RED.as_rgba_f32(),
+        	Color::GREEN.as_rgba_f32(),
+        	Color::BLUE.as_rgba_f32(),
+        	Color::WHITE.as_rgba_f32(),
+		], 
+	);
+	
+	println!("Quad created!");
+	
+	mesh
+}
