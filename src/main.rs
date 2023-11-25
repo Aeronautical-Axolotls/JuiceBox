d2322bef421efaee250b2e3a9f52840b94d869cd
kadealicious <kadesamson@gmail.com>
2023-11-24 22:10:53 -0800

    Sim state manager boilerplate, method stubs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use bevy::prelude::*;
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+// use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
-pub mod test;
+pub mod sim_state_manager;
+// pub mod test;
 
 fn main() {
     let mut juicebox: App = App::new();
-	juicebox.add_plugins
-	((
+	juicebox.add_plugins((
 		DefaultPlugins, 
-		test::HelloWorld, 
+		sim_state_manager::SimStateManager, 
 		
-		// Debug plugins: 
-		LogDiagnosticsPlugin::default(), 
-		FrameTimeDiagnosticsPlugin::default(), 
+		// Non-release plugins: 
+		// test::HelloWorld, 
+		// LogDiagnosticsPlugin::default(), 
+		// FrameTimeDiagnosticsPlugin::default(), 
 	));
 	juicebox.run();
 }
