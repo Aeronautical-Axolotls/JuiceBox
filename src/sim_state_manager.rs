5c361c01a498d2e26c2c0841176f0a7d8908b6a2
r3dc0g <critesg@spu.edu>
2023-12-01 15:01:55 -0800

    Completed JUICE-52

--- src/sim_state_manager.rs
+++ src/sim_state_manager.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::math::Vec2;
+use anyhow::Result;
 
 pub struct SimStateManager;
 impl Plugin for SimStateManager {
@@ -7,7 +8,7 @@ impl Plugin for SimStateManager {
 		app.insert_resource(SimConstraints::default());
 		app.insert_resource(SimParticles::default());
 		app.insert_resource(SimGrid::default());
-		
+
 		app.add_systems(Startup, setup);
 		app.add_systems(Update, update);
 	}
@@ -15,26 +16,26 @@ impl Plugin for SimStateManager {
 
 // Simulation state manager initialization code.
 fn setup(
-	mut _commands:		Commands, 
-	mut _constraints:	ResMut<SimConstraints>, 
-	mut _grid:			ResMut<SimGrid>, 
+	mut _commands:		Commands,
+	mut _constraints:	ResMut<SimConstraints>,
+	mut _grid:			ResMut<SimGrid>,
 	mut _particles:		ResMut<SimParticles>) {
-	
+
 	println!("Initializing state manager...");
-	
+
 	// TODO: Get saved simulation data from most recently open file OR default file.
 	// TODO: Population constraints, grid, and particles with loaded data.
-	
+
 	println!("State manager initialized!");
 }
 
 // Simulation state manager update; handles user interactions with the simulation.
 fn update(
-	mut _commands:		Commands, 
-	mut _constraints:	ResMut<SimConstraints>, 
-	mut _grid:			ResMut<SimGrid>, 
+	mut _commands:		Commands,
+	mut _constraints:	ResMut<SimConstraints>,
+	mut _grid:			ResMut<SimGrid>,
 	mut _particles:		ResMut<SimParticles>) {
-	
+
 	// TODO: Check for and handle simulation saving/loading.
 	// TODO: Check for and handle simulation pause/timestep change.
 	// TODO: Check for and handle changes to simulation grid.
@@ -64,33 +65,41 @@ struct SimConstraints {
 impl Default for SimConstraints {
 	fn default() -> SimConstraints {
 		SimConstraints {
-			_grid_particle_ratio:	0.1, 
-			_iterations_per_frame:	5, 
-			_gravity_direction:		270, 
-			_gravity_strength:		9.81, 
+			_grid_particle_ratio:	0.1,
+			_iterations_per_frame:	5,
+			_gravity_direction:		270,
+			_gravity_strength:		9.81,
 		}
 	}
 }
 
-enum SimGridCellType	{ Air, _Fluid, _Solid, }
+#[derive(Clone)]
+enum SimGridCellType	{ Air, Fluid, Solid, }
+
 #[derive(Resource)]
 struct SimGrid {
-	_dimensions:	[u16; 2], 			// Number of grid cells as [x, y].
-	_cell_size:		u8, 				// Grid cell size (lower size -> higher precision).
-	_cell_type:		SimGridCellType,
-	_velocity:		Vec<[Vec2; 4]>, 	// Velocities for each grid cell at all 4 edges.
+	dimensions:	    [u16; 2],
+	cell_size:		u8,
+	cell_type:		Vec<SimGridCellType>,
+	velocity:		Vec<[Vec2; 4]>,
 }
+
 impl Default for SimGrid {
 	fn default() -> SimGrid {
 		SimGrid {
-			_dimensions:	[250, 250], 
-			_cell_size:		10, 
-			_cell_type:		SimGridCellType::Air, 
-			
-			// BUG: Not sure if this is correct.
-			_velocity:		Vec::new(), 
+			dimensions:	[250, 250],
+			cell_size:		10,
+			cell_type:		vec![SimGridCellType::Air; 625],
+			velocity:		vec![[Vec2::new(0.0, 0.0); 4]; 625],
 		}
 	}
+}
+
+impl SimGrid {
+    pub fn _set_grid_cell_type(&mut self, cell_index: usize, cell_type: SimGridCellType) -> Result<()> {
+        self.cell_type[cell_index] = cell_type;
+        Ok(())
+    }
 }
 
 #[derive(Resource)]
@@ -102,9 +111,9 @@ struct SimParticles {
 impl Default for SimParticles {
 	fn default() -> SimParticles {
 		SimParticles {
-			_particle_count:	0, 
-			_position:			Vec::new(), 
-			_velocity:			Vec::new(), 
+			_particle_count:	0,
+			_position:			Vec::new(),
+			_velocity:			Vec::new(),
 		}
 	}
 }
