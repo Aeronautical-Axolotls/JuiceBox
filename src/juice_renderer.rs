544ec99fe832694aac1f2a1f04e5570badb530f7
kadealicious <kadesamson@gmail.com>
2023-12-04 18:41:49 -0800

    Added color blending function for color mapping

--- src/juice_renderer.rs
+++ src/juice_renderer.rs
@@ -1,30 +1,55 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::{Window, WindowPlugin};
-
+use bevy::core_pipeline::prelude::ClearColor;
+use bevy::render::RenderApp;
 use crate::juice_math::generate_random_usize;
+use crate::sim_state_manager::SimParticles;
+
+pub const WINDOW_WIDTH: f32		= 640.0;
+pub const WINDOW_HEIGHT: f32	= 480.0;
+
+/// Color definitions!
+pub const JUICE_BLUE: Color		= Color::rgb(0.66, 0.91, 1.0);
+pub const JUICE_AMBER: Color	= Color::rgb(1.0, 0.73, 0.17);
 
 pub struct JuiceRenderer;
 impl Plugin for JuiceRenderer {
 	
 	fn build(&self, app: &mut App) {
+		app.insert_resource(ClearColor(JUICE_AMBER));
+		
 		app.add_systems(Startup, setup_renderer);
+		
+		let mut render_app = app.sub_app_mut(RenderApp);
+		// render_app.add_systems(Render, something_probably_goes_here_but_idk_what_yet);
 	}
 }
 
 /// Custom rendering pipeline initialization.
-fn setup_renderer(mut _commands: Commands) {
+fn setup_renderer(
+	mut commands:	Commands,
+	mut meshes:		ResMut<Assets<Mesh>>,
+	mut materials:	ResMut<Assets<ColorMaterial>>, 
+	mut particles:	ResMut<SimParticles>) {
 	
-}
-
-pub struct ParticleSystem {
-	pub rendered_texture: Handle<Image>, 
+	// Spawn a camera to view our simulation world!
+	commands.spawn(Camera2dBundle::default());
+	
+	// Spawn test particle!
+	commands.spawn(MaterialMesh2dBundle {
+		mesh:		meshes.add(shape::Circle::new(50.0).into()).into(),
+		material:	materials.add(ColorMaterial::from(JUICE_BLUE)),
+		transform:	Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+		..default()
+	});
 }
 
 /// Create a window plugin to add into Bevy's default plugins suite.
 pub fn create_window_plugin() -> WindowPlugin {
 	let window_handle: Window = Window {
 		position:	WindowPosition::Centered(MonitorSelection::Primary), 
-		resolution:	(640.0, 480.0).into(), 
+		resolution:	(WINDOW_WIDTH, WINDOW_HEIGHT).into(), 
 		title:		create_window_title("JuiceBox"), 
 		..default()
 	};
@@ -40,17 +65,13 @@ pub fn create_window_plugin() -> WindowPlugin {
 /// Create a window title with a fun message appended to the title parameter.
 pub fn create_window_title(title: &str) -> String {
 	// Strings to be appended to the window title parameter!
-	let silly_strings: [&str; 10]	= [
+	let silly_strings: [&str; 6]	= [
 		"Spilling encouraged!", 
 		"Don't cry over spilled milk!", 
-		"Rolling in the deep!", 
-		"Don't drink the toilet water!", 
-		"We're not fans of dry humor...", 
+		"Drinking toilet water since 2023!", 
 		"Rivers Cuomo loves it!", 
-		"Cry me a river!", 
-		"Styx and stones!", 
+		"Domo Arigato, Mr. Roboto!", 
 		"Hydrate or diedrate!", 
-		"Liquid fun!", 
 	];
 	
 	let title_length: usize	= title.len();
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     let mut juicebox: App = App::new();
 	juicebox.add_plugins((
 		DefaultPlugins.set(juice_renderer::create_window_plugin()),
+		juice_renderer::JuiceRenderer,
 		sim_state_manager::SimStateManager,
 		sim_physics_engine::SimPhysicsEngine,
-		juice_renderer::JuiceRenderer, 
 
 		// Non-release plugins:
 		// test::HelloWorld,
