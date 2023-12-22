34ef70af5082ade1fce133909481532cf8f2fba7
kadealicious <kadesamson@gmail.com>
2023-12-21 22:15:02 -0800

    Began make_grid_velocities_incompressible()

--- src/simulation/sim_physics_engine.rs
+++ src/simulation/sim_physics_engine.rs
@@ -75,11 +75,47 @@ fn particles_to_grid(mut grid: ResMut<SimGrid>, particles: Query<(Entity, &mut S
 
 }
 
+/// Force velocity incompressibility for each grid cell within the simulation.
+pub fn make_grid_velocities_incompressible(mut grid: ResMut<SimGrid>) {
+	
+	// For each grid cell, make the fluid incompressible.
+	for x in 0..grid.dimensions.0 {
+		for y in 0..grid.dimensions.1 {
+			
+			// Calculate divergence for this cell.
+			let left_flow: f32	= grid.velocity_u[x as usize][y as usize];
+			let right_flow: f32	= grid.velocity_u[(x + 1) as usize][y as usize];
+			let up_flow: f32	= grid.velocity_v[x as usize][y as usize];
+			let down_flow: f32	= grid.velocity_v[x as usize][(y + 1) as usize];
+			let divergence: f32	= calculate_divergence(left_flow, right_flow, up_flow, down_flow);
+			
+			
+			
+			// =================================
+			// TODO: Factor in solid collisions.
+			// =================================
+			
+			
+			
+			// Factor in any collisions with surrounding solid cells.
+			// let solid_factor: f32			= 
+			let divergence_per_side: f32	= divergence / 4.0;
+			
+			// Force incompressibility on this cell.
+			grid.velocity_u[x as usize][y as usize]			+= divergence_per_side;
+			grid.velocity_u[(x + 1) as usize][y as usize]	-= divergence_per_side;
+			grid.velocity_v[x as usize][y as usize]			-= divergence_per_side;
+			grid.velocity_v[x as usize][(y + 1) as usize]	+= divergence_per_side;
+		}
+	}
+}
+
 /** Calculate the divergence (inflow/outflow) of a grid cell.  If this number is not zero, then 
-	the fluid must be made incompressible.  **A positive divergence indicates there is too much 
-	inflow, whereas a negative divergence indicates too much outflow.** */
+	the fluid must be made incompressible.  **A negative divergence indicates there is too much 
+	inflow, whereas a positive divergence indicates too much outflow.** */
 fn calculate_divergence(left_flow: f32, right_flow: f32, up_flow: f32, down_flow: f32) -> f32 {
 	
-	let divergence: f32 = (left_flow - right_flow) + (down_flow - up_flow);
+	// BUG: The up and down flows may need to be reversed.
+	let divergence: f32 = (right_flow - left_flow) + (up_flow - down_flow);
 	divergence
 }
